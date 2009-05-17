6e4c2edd6957f48263e3a0b713b39588b6c78d96
Niklas Hofer <niklas+dev@lanpartei.de>
2009-05-17 14:56:34 +0200

    filter files by substring

--- lib/window.rb
+++ lib/window.rb
@@ -30,20 +30,19 @@ module Tomb
       filelist.update
     end
 
-    def on_filter_changed
-      STDERR.puts "filtering.."
+    def on_filter_changed(entry)
+      filelist.filter_string = entry.text
     end
   end
 
   class FileList
     attr_reader :store, :view, :gibak
+    attr_reader :filtered_store, :filter_string
     Path = 0
     def initialize(container)
-      @store = Gtk::ListStore.new(String)
-      @store.set_default_sort_func {|a,b| a[Path] <=> b[Path] }
-      @view = Gtk::TreeView.new(@store)
-      container.add view
+      setup_store
       setup_tree_view
+      container.add view
       @gibak = Gibak::Base.new
     end
 
@@ -55,11 +54,33 @@ module Tomb
       end
     end
 
+    def filter_string=(new_filter_string)
+      @filter_string = new_filter_string
+      filtered_store.refilter
+    end
+
     def setup_tree_view
+      @view = Gtk::TreeView.new(filtered_store)
       renderer = Gtk::CellRendererText.new
       column = Gtk::TreeViewColumn.new("Path", renderer, :text => Path)
       view.append_column(column)
       view
+    end
+
+    def setup_store
+      @store          = Gtk::ListStore.new(String)
+      @filtered_store = Gtk::TreeModelFilter.new(store)
+
+      store.set_default_sort_func {|a,b| a[Path] <=> b[Path] }
+
+      @filter_string = ''
+      filtered_store.set_visible_func do |model, iter|
+        path_visible? iter[Path]
+      end
+    end
+
+    def path_visible?(path)
+      filter_string.blank? || path.include?(filter_string)
     end
 
     def destroy
